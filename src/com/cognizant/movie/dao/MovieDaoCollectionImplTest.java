87f4503d89f15e5f9c61927db86aafc5eb3ce57e
835190-rishika <vkrishika1998@gmail.com>
2020-01-07 17:43:29 +0530

    Java

--- src/com/cognizant/movie/dao/MovieDaoCollectionImplTest.java
+++ src/com/cognizant/movie/dao/MovieDaoCollectionImplTest.java
@@ -37,8 +37,8 @@ public class MovieDaoCollectionImplTest {
 
     public static void testmodifyMovieLists() {
         System.out.println("Modify Movie");
-        Movie movie = new Movie(1, "It", 2787985087L, true, DateUtil.convertToDate("02/01/2020"),
-                "superhero", false);
+        Movie movie = new Movie(1, "Conjuring", 2787985687L, true, DateUtil.convertToDate("02/01/2020"),
+                "horror", false);
         moviedao.modifyMovieLists(movie);
     }
 
